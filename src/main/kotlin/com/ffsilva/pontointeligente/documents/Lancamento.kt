2fb485dddd571c8679f69431c8fd67e96b950d06
Francisco Netto <fsilvafrancisco16@gmail.com>
2019-05-31 00:48:51 -0300

    Corrige nome da classe.

--- src/main/kotlin/com/ffsilva/pontointeligente/documents/Lancamento.kt
+++ src/main/kotlin/com/ffsilva/pontointeligente/documents/Lancamento.kt
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
 @Document
-data class Lacamento(
+data class Lancamento(
         @Id val id: String? = null,
 
         val data: LocalDateTime,
